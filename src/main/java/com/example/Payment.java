a63a65014b0e9faf49f246b9a352f4b70d7b3424
testmail-ihub <58020879+testmail-ihub@users.noreply.github.com>
2025-11-04 18:09:46 +0530

    Add abstract Payment class with common fields and processPayment() method

---
+++ src/main/java/com/example/Payment.java
@@ -0,0 +1,21 @@
+package com.example;
+
+public abstract class Payment {
+    protected double amount;
+    protected String currency;
+
+    public Payment(double amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public abstract void processPayment();
+}
